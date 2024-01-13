7436859a0c81eeaf7049d4cc9acd5e61b639a84d
Lancelot Robson <lancelot.robson@gmail.com>
2024-01-13 15:30:38 +0000

    Abortive attempt at python test

--- firebot/led_rainbow.py
+++ firebot/led_rainbow.py
@@ -2,6 +2,8 @@ import time
 from pimoroni import Button
 from plasma import WS2812
 from motor import motor2040
+import select
+import sys
 
 """
 Displays a rotating rainbow pattern on the Motor 2040's onboard LED.
@@ -29,15 +31,29 @@ led.start()
 
 hue = 0.0
 
+# setup poll to read USB port
+poll_object = select.poll()
+poll_object.register(sys.stdin,1)
+command = ""
 # Make rainbows until the user button is pressed
 while not user_sw.raw():
 
     hue += SPEED / 1000.0
 
     # Update the LED
-    led.set_hsv(0, hue, 1.0, BRIGHTNESS)
+    # led.set_hsv(0, hue, 1.0, BRIGHTNESS)
 
     time.sleep(1.0 / UPDATES)
+    if poll_object.poll(0):
+        ch = sys.stdin.read(1)
+        if ch == "\n":
+            # Got a command terminator
+            if command != "":
+                tokens = command.split(" ")
+                if len(tokens) == 4 and tokens[0] == "+sa":
+                    led.set_rgb(int(tokens[1]), int(tokens[2]), int(tokens[3]))
+        else:
+            command = f"{command}{ch}"
 
 # Turn off the LED
 led.clear()
